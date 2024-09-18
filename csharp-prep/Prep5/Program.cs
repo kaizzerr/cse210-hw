fafe62e4d014de6f701bf5e3e85b82bf99fcc372
kaizzerr <kaizzergatan@gmail.com>
2024-09-18 08:39:47 -0600

    Prep 5 Project

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,12 +6,9 @@ class Program
     static void Main(string[] args)
     {
         DisplayWelcomeMessage();
-
         string userName = PromptUserName();
         int userNumber = PromptUserNumber();
-
         int squaredNumber = SquareNumber(userNumber);
-
         DisplayResult(userName, squaredNumber);
     }
 
