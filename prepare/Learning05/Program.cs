97ae3d2de8dbf789a93e1d81831b6e0f01fdb6f4
kaizzerr <kaizzergatan@gmail.com>
2024-10-14 18:53:56 -0600

    Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("Kaizzer Gatan", "C++");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Richard Reyes", "Chemistry", "2.1", "10-30");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Sunny Roberts", "Social Studies", "Canadian Heritage");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
