37764ec27224e3174dc45e41f5df442a94cd69af
kaizzerr <kaizzergatan@gmail.com>
2024-10-20 20:07:15 -0600

    Learning 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Pink", 7);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Purple", 3, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Cyan", 4);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
